c29abdb69907f3fe7e96a0fd24937687a4ed5713
Brian Zanti <brianzanti@gmail.com>
2018-06-25 14:14:47 -0600

    refactors

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,28 +1,11 @@
 class CreditCheck
+
+
   def valid_number?(card_number)
 
-    double_every_other = []
-    card_number.to_s.split("").reverse.each.with_index do |number, index|
-      if(index.odd?)
-        double_every_other << number.to_i * 2
-      else
-        double_every_other << number.to_i
-      end
-    end
-
-    summed_over_ten = double_every_other.map do |digit|
-      if(digit >= 10)
-        digit - 9
-      else
-        digit
-      end
-    end
-
-    sum = 0
-    summed_over_ten.each do |num|
-      sum += num
-    end
-
+    doubled = double_every_other(card_number)
+    summed_over_ten = sum_over_ten(doubled)
+    sum = summed_over_ten.sum
     if sum % 10 == 0
       return true
     else
@@ -35,6 +18,27 @@ class CreditCheck
       return "The number #{card_number} is valid"
     else
       return "The number #{card_number} is invalid"
+    end
+  end
+
+  def double_every_other(card_number)
+    digits = card_number.to_s.split("").reverse
+    digits.map.with_index do |number, index|
+      if(index.odd?)
+        number.to_i * 2
+      else
+        number.to_i
+      end
+    end
+  end
+
+  def sum_over_ten(digits)
+    digits.map do |digit|
+      if(digit >= 10)
+        digit - 9
+      else
+        digit
+      end
     end
   end
 end
