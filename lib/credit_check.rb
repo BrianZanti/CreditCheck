5ae12d5bb2d4031efe90fbc77bda9b335be1d19f
Brian Zanti <brianzanti@gmail.com>
2018-06-29 08:27:11 -0600

    changes map to each

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,8 +1,5 @@
 class CreditCheck
-
-
   def valid_number?(card_number)
-
     doubled = double_every_other(card_number)
     summed_over_ten = sum_over_ten(doubled)
     sum = summed_over_ten.sum
@@ -23,22 +20,26 @@ class CreditCheck
 
   def double_every_other(card_number)
     digits = card_number.to_s.split("").reverse
-    digits.map.with_index do |number, index|
+    doubled_every_other = []
+    digits.each.with_index do |number, index|
       if(index.odd?)
-        number.to_i * 2
+        doubled_every_other << number.to_i * 2
       else
-        number.to_i
+        doubled_every_other << number.to_i
       end
     end
+    doubled_every_other
   end
 
   def sum_over_ten(digits)
-    digits.map do |digit|
+    summed_over_ten = []
+    digits.each do |digit|
       if(digit >= 10)
-        digit - 9
+        summed_over_ten << digit - 9
       else
-        digit
+        summed_over_ten << digit
       end
     end
+    summed_over_ten
   end
 end
